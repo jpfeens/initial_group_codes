23a65287409828f8a39a5fdb7772956efa0c8cc8
ertoddgolder <ertodd@golder.co.nz>
2021-03-15 11:36:59 +1300

    Cleaned up file structure and added more __pycache__ files to .gitignore

--- python_tools/deterministic_tools.py
+++ python_tools/deterministic_tools.py
@@ -5,7 +5,7 @@ Created on Wed Feb 24 14:28:33 2021
 @author: ErTodd
 """
 
-def calculate_ground_motions(gmm_list, desired_calculations, selected_fault, sctx, rctx, dctx, imts, stddev_types):
+def calculate_ground_motions(gmm, sctx, rctx, dctx, imts, stddev_types):
     '''
     Calculates the expected ground motion and uncertainty, organised by GMPE
     and intensity measure type (i.e. PGA, SA etc.), for a given rupture-site configuration
@@ -14,10 +14,6 @@ def calculate_ground_motions(gmm_list, desired_calculations, selected_fault, sct
     ----------
     gmm_list : LIST
         List of openquake GMM classes. Obtained from get_available_gsims().
-    desired_calculations : LIST
-        List of the calculation outputs deisred. e.g. ['mean', 'stddevs', 'mean_plus_1sd', 'mean_minus_1sd']
-    selected_fault : STR
-        Fault name for calculation.
     sctx : Class
         OpenQuake SitesContext class containing site information. e.g., Vs30, Z1pt0, etc.
     rctx : Class
@@ -33,33 +29,30 @@ def calculate_ground_motions(gmm_list, desired_calculations, selected_fault, sct
     Returns
     -------
     gmm_ground_motions : DICT
-        Dictionary containing calculated ground motion values for the desired calculations 
-        and each intensity measure type in imts.
+        Dictionary containing calculated ground motion values (in units of acceleration) 
+        for the desired calculations and each intensity measure type in imts.
 
     '''
 
     import numpy as np
-    gmm_ground_motions = dict.fromkeys([str(x) for x in gmm_list])
     nper = len(imts)
-    for gmm in gmm_list:
-        gmm_ground_motions[gmm] = {"mean": np.zeros(nper),
-                                  "stddevs": np.zeros(nper),
-                                  "mean_plus_1sd": np.zeros(nper),
-                                  "mean_minus_1sd": np.zeros(nper)}            
-            
-        for i, imt in enumerate(imts):
-            try:
-                mean, [stddev] = gmm.get_mean_and_stddevs(sctx, rctx, dctx, imt, stddev_types)
-                gmm_ground_motions[gmm]['mean'][i] = np.exp(mean)
-                gmm_ground_motions[gmm]['stddevs'][i] = stddev
-                gmm_ground_motions[gmm]['mean_plus_1sd'][i] = np.exp(mean + stddev)
-                gmm_ground_motions[gmm]['mean_minus_1sd'][i] = np.exp(mean - stddev)
+    gmm_ground_motions = {"mean": np.zeros(nper),
+                              "stddevs": np.zeros(nper),
+                              "mean_plus_1sd": np.zeros(nper),
+                              "mean_minus_1sd": np.zeros(nper)}
+    for i, imt in enumerate(imts):
+        try:
+            mean, [stddev] = gmm.get_mean_and_stddevs(sctx, rctx, dctx, imt, stddev_types)
+            gmm_ground_motions['mean'][i] = np.exp(mean)
+            gmm_ground_motions['stddevs'][i] = stddev
+            gmm_ground_motions['mean_plus_1sd'][i] = np.exp(mean + stddev)
+            gmm_ground_motions['mean_minus_1sd'][i] = np.exp(mean - stddev)
 
-            except KeyError:
-                # If it raises the error we have just seen (a KeyError) - put in nans
-                gmm_ground_motions[gmm]['mean'][i] = np.nan
-                gmm_ground_motions[gmm]['stddevs'][i] = np.nan
-                gmm_ground_motions[gmm]['mean_plus_1sd'][i] = np.nan
-                gmm_ground_motions[gmm]['mean_minus_1sd'][i] = np.nan
+        except KeyError:
+            # If it raises the error we have just seen (a KeyError) - put in nans
+            gmm_ground_motions['mean'][i] = np.nan
+            gmm_ground_motions['stddevs'][i] = np.nan
+            gmm_ground_motions['mean_plus_1sd'][i] = np.nan
+            gmm_ground_motions['mean_minus_1sd'][i] = np.nan
                 
     return gmm_ground_motions
--- python_tools/seismic_catalogue_tools.py
+++ python_tools/seismic_catalogue_tools.py
@@ -42,7 +42,9 @@ def get_gcmt_catalogue(gcmt_outfile, catalogue_raw_files, raw_data_dir, search_p
 
     if os.path.isfile(os.path.join(raw_data_dir, outfile)) == True:
         print('Outfile exists.')
-        shutil.copy(os.path.join(raw_data_dir, outfile),catalogue_raw_files.get('GCMT')[0])
+        run = 1
+        if 'GCMT' in catalogue_raw_files:
+            shutil.copy(os.path.join(raw_data_dir, outfile),catalogue_raw_files.get('GCMT')[0])
     else:
         #----------------------------------------------------#
         #code starts
@@ -124,6 +126,193 @@ def get_gcmt_catalogue(gcmt_outfile, catalogue_raw_files, raw_data_dir, search_p
                 #export.write(line)    
             export.write("End of events found with given criteria.")
 
-        shutil.copy(os.path.join(raw_data_dir, outfile),catalogue_raw_files.get('GCMT')[0])
+        if 'GCMT' in catalogue_raw_files:
+            shutil.copy(os.path.join(raw_data_dir, outfile),catalogue_raw_files.get('GCMT')[0])
 
     return run
+
+def magnitude_conversions(scale, mag):
+    
+    from math import exp
+     # mb convert  to mw using exponential models from ISC-GEM Report 201201-V01 (Storchak et al. 2012)
+    if scale == "mb" or scale == "mb1mx" or scale == "mbtmp" or scale == "mb1":
+        if mag > 6.8:
+            mag = mag #NO - this completely ignores mb saturation and will underestimate Mw proxy values
+            scale = "mw"
+        elif mag > 5 and mag <= 6.8:
+            mag = round(exp(-4.66+0.86*mag) + 4.56,1)
+            scale = "mw"
+        else:
+            mag = round(0.85*mag+1.03,1)
+            scale = "mw"
+           
+    # ms convert to mw using exponential models from ISC-GEM Report 201201-V01 (Storchak et al. 2012)
+    #mv is a surface wave magnitude defined by Vanek et al. ( 1962)
+    elif scale == "ms" or scale == "ms1mx" or scale == "ms7" or scale == "ms1" or scale == "msz" or scale == "mv":     
+        mag = round(exp(-0.22+0.23*mag) + 2.86,1) #checked against Storchak et al. 2012 on 9/3/2020
+        scale = "mw"           
+   
+    #convert mags to mw using Rong et al. 2015 eqns
+    elif scale == "ml" or scale == "mlv":
+        mag = round(1.3210*mag - 1.8631, 1)
+        scale = "mw"
+        
+    # convert Brazillian mR to mw following de Almeida et al. 2018
+    elif scale == 'mr':
+        mag = mag + 0.34
+        scale = 'mw'
+       
+    #take other magnitudes as Mw
+    elif scale == "m" or scale == "md" or scale == "uk" or scale == "mg" or scale == "mu" or scale == "unk" or scale == 'mind' or scale == 'ma' or scale == 'mm' or scale == 'mi':
+        mag = mag
+        scale = "mw"
+       
+    #Japan magnitude scale from https://agupubs.onlinelibrary.wiley.com/doi/10.1002/2017JB014697
+    elif scale == "mj":
+            a = 0.053
+            b = 0.33
+            c = 1.68
+            mag = round(a*mag**2 + b*mag + c ,1)
+       
+    elif scale == "mw" or scale == "mw(mb)":
+            pass
+        
+    return scale, mag
+
+def organise_catalogues(Compile=1,Duplicate=1):
+    
+    #Create lists for TimeVal function
+    #Number of seconds from beginning of year to beginning of month (non-leap year)
+    monthlengths=[31,28,31,30,31,30,31,31,30,31,30]
+    for i in range(1,12):
+        msec.append(msec[i-1]+monthlengths[i-1]*24*60**2)
+    #Number of seconds from beginning of year1 to beginning of years after (beginning with year1)
+    for i in range(year1,yearlast):
+        ysec.append(ysec[i-year1]+365*24*60**2)
+        if ((i%4==0 and i%100!=0) or i%400==0):
+            ysec[i-year1+1]=ysec[i-year1+1]+24*60**2
+    #
+
+    # Compile: extract eq data from verious agencies
+    # Duplicate: locate duplicate events among various events
+    time_min=TimeVal(year1,1,1,0,0,0)
+    time_max=TimeVal(yearlast,1,1,0,0,0)
+#    lat_min,lat_max = -17, 3 #-43, -23  #-7, 5
+#    lon_min,lon_max = -82.5, -68.5 #-81, -61 #115, 128
+    m_min = min_magnitude
+    m_max = 10
+    #
+    if Compile==1:
+
+        infile=catalogue_raw_files.get('BRAZIL')[0]
+        outfile=catalogue_raw_files.get('BRAZIL')[1]
+        OrganizeBRAZIL(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+
+        #infile=catalogue_raw_files.get('Chile')[0]
+        #outfile=catalogue_raw_files.get('Chile')[1]
+        #OrganizeCHILE(infile,outfile,chile_lat_min,chile_lat_max,lon_min,lon_max,m_min)
+        
+        #VELOSO format is copied from SARA
+        infile=catalogue_raw_files.get('VELOSO')[0]
+        outfile=catalogue_raw_files.get('VELOSO')[1]
+        OrganizeSARA(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+        
+        infile=catalogue_raw_files.get('SARA')[0]
+        outfile=catalogue_raw_files.get('SARA')[1]
+        OrganizeSARA(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+
+        infile=catalogue_raw_files.get('ANSS')[0]
+        outfile=catalogue_raw_files.get('ANSS')[1]
+        OrganizeANSS(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+
+
+        infile=catalogue_raw_files.get('GCMT')[0]
+        outfile=catalogue_raw_files.get('GCMT')[1]
+        OrganizeCMT(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+
+        infile=catalogue_raw_files.get('ISCGEM')[0]
+        outfile=catalogue_raw_files.get('ISCGEM')[1]
+        OrganizeISC(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+
+        infile=catalogue_raw_files.get('GEM_Historic')[0]
+        outfile=catalogue_raw_files.get('GEM_Historic')[1]
+        OrganizeGEMHistoric(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+
+        infile=catalogue_raw_files.get('Centennial')[0]
+        outfile=catalogue_raw_files.get('Centennial')[1]
+        OrganizeCentnnl(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+        
+#        infile="RawData/NOAA_catalog_use.csv"
+#        outfile="OrganizedData/8-NOAA_OUT.txt"
+#        OrganizeNOAA(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+        
+#        infile="RawData/Iris_cat_OTMine_08012018_use.csv"
+#        outfile="OrganizedData/2-IRIS_OUT.txt"
+#        OrganizeIRIS(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+        
+#                
+#        infile="RawData/GEM_global_historical_eqks.csv"
+#        outfile="OrganizedData/3-GEMHistoric_OUT.txt"
+#        OrganizeGEMHistoric(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+#        
+##        infile="RawData/SCR_Catalog_Original.csv"
+##        outfile="OrganizedData/6-SCR_OUT.txt"
+##        OrganizeOthers(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+#
+#        
+##        infile="RawData/ISC_catalog_OTmine_07312018.csv"
+##        outfile="OrganizedData/8-ISC_OUT.txt"
+##        OrganizeISC2(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+#
+#        infile="RawData/NOAA_catalog_use.csv"
+#        outfile="OrganizedData/5-NOAA_OUT.txt"
+#        OrganizeNOAA(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+#       
+#        infile="RawData/GFZmt_cat_OTmine_07312018.csv"
+#        outfile="OrganizedData/11-GeoFon_OUT.txt"
+#        OrganizeOthers(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+#        
+#        infile="RawData/ChinaUniformedEarthquakeData_use.csv"
+#        outfile="OrganizedData/12-China1_OUT.txt"
+#        OrganizeOthers2(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+#        
+#        infile="RawData/ChinaEarthquakeData_use.csv"
+#        outfile="OrganizedData/13-China2_OUT.txt"
+#        OrganizeOthers2(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+#        
+#        infile="RawData/XuEtal_2016_use.csv"
+#        outfile="OrganizedData/14-XuEtal2016_OUT.txt"
+#        OrganizeOthers(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+#        
+#        infile="RawData/EMSC_cat_OTmine_20180802.csv"
+#        outfile="OrganizedData/15-EMSC_OUT.txt"
+#        OrganizeOthers2(infile,outfile,lat_min,lat_max,lon_min,lon_max,m_min)
+
+
+
+ 
+    if Duplicate==1:
+        
+        t_window=15 #s
+        d_window=100 #km
+        m_window=1 #magnitude
+        infile_list = [x[1] for x in catalogue_raw_files.values()]
+#        infile_list = ["OrganizedData/1-Peru_OUT.txt",
+#                       "OrganizedData/2-Chile_OUT.txt",
+#                       "OrganizedData/3-SARA_OUT.txt",
+#                       "OrganizedData/4-ANSS_OUT.txt",
+#                       "OrganizedData/5-GCMT_OUT.txt",
+#                       "OrganizedData/6-ISCGEM_OUT.txt",
+#                       "OrganizedData/7-GEMHistoric_OUT.txt",
+#                       "OrganizedData/8-Centennial_OUT.txt",                     
+#                       "OrganizedData/8-NOAA_OUT.txt"
+#                       "OrganizedData/11-GeoFon_OUT.txt",
+#                       "OrganizedData/12-China1_OUT.txt",
+#                       "OrganizedData/13-China2_OUT.txt",
+#                       "OrganizedData/14-XuEtal2016_OUT.txt",
+#                       "OrganizedData/15-EMSC_OUT.txt"
+#                       ]
+        outfile = combined_catalogue_file
+        CombineWithoutDuplicates(infile_list,outfile,t_window,d_window,m_window)
+#        outfile = "Tenke-Combined-nodups-oldmethod.csv"
+#        DuEvent(infile_list,outfile,t_window,d_window,m_window)
